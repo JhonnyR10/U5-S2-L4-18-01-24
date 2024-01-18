09f5587620b39c59c3b5f1defccf87dbcc6b06b3
Giovanni Longo <giovanni.longo10@icloud.com>
2024-01-18 16:54:28 +0100

    first commit

---
+++ src/test/java/giovannilongo/U5S2L4180124/U5S2L4180124ApplicationTests.java
@@ -0,0 +1,13 @@
+package giovannilongo.U5S2L4180124;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S2L4180124ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
